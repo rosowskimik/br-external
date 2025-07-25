34a0b873384bad51c3b9ca5e6b00d8b617b6a344
Mikołaj Rosowski <m.rosowski1@wp.pl>
2025-07-25 23:32:16 +0200

    boards: update post-build script
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- board/rpi/post-build.sh
+++ board/rpi/post-build.sh
@@ -23,4 +23,6 @@ cp -r ${linux_dir}/arch/arm/boot/dts/overlays/*.dtbo ${BINARIES_DIR}/rpi-firmwar
 cp -r ${linux_dir}/arch/arm/boot/dts/overlays/README ${BINARIES_DIR}/rpi-firmware/overlays
 
 # Boot directly from initramfs
-rm -fv ${TARGET_DIR}/etc/initrd-release
+if [ -d ${TARGET_DIR}/etc/systemd ]; then
+  rm -fv ${TARGET_DIR}/etc/initrd-release
+fi
