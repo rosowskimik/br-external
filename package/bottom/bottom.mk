e035caefd1a0e7dfdc45bd1e98cf9ca6b54adf0c
Mikołaj Rosowski <m.rosowski1@wp.pl>
2025-10-15 13:33:33 +0200

    package/bottom: new package
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

---
+++ package/bottom/bottom.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# bottom
+#
+################################################################################
+
+BOTTOM_VERSION = 0.11.2
+BOTTOM_SITE = $(call github,ClementTsang,bottom,0.11.2)
+BOTTOM_LICENSE = MIT
+BOTTOM_LICENSE_FILES = LICENSE
+
+ifeq ($(BR2_PACKAGE_BASH_COMPLETION),y)
+BOTTOM_CARGO_ENV += "BTM_GENERATE=true"
+endif
+
+$(eval $(cargo-package))
