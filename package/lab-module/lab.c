fb65ace238982a256b4a7eacb13efa2aba15804f
Mikołaj Rosowski <m.rosowski1@wp.pl>
2025-10-29 13:39:08 +0100

    package/lab-module: print levels
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- package/lab-module/lab.c
+++ package/lab-module/lab.c
@@ -4,7 +4,13 @@
 
 static int __init lab_init(void)
 {
-	pr_info("Module init\n");
+	pr_emerg("emerg\n");
+	pr_alert("alert\n");
+	pr_crit("crit\n");
+	pr_warn("warn\n");
+	pr_notice("notice\n");
+	pr_info("info\n");
+	pr_debug("debug\n");
 	return 0;
 }
 
