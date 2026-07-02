0f64948f183ffc1f2e495484dfd0ef24bae4ac04
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-07-02 22:47:24 +0200

    package/bottom: bump version

--- package/bottom/bottom.mk
+++ package/bottom/bottom.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-BOTTOM_VERSION = 0.11.2
+BOTTOM_VERSION = 0.14.2
 BOTTOM_SITE = $(call github,ClementTsang,bottom,$(BOTTOM_VERSION))
 BOTTOM_LICENSE = MIT
 BOTTOM_LICENSE_FILES = LICENSE
 
 ifeq ($(BR2_PACKAGE_BASH_COMPLETION),y)
-BOTTOM_CARGO_ENV += "BTM_GENERATE=true"
+BOTTOM_CARGO_ENV += BTM_GENERATE=true
 endif
 
 $(eval $(cargo-package))
