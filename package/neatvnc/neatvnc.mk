c409c53be1b4c1357b1cd4fc88b79ab4e7f219b9
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-08-30 14:26:47 +0200

    package/neatvnc: bump version

--- package/neatvnc/neatvnc.mk
+++ package/neatvnc/neatvnc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEATVNC_VERSION = 1.0.0
+NEATVNC_VERSION = 1.0.1
 NEATVNC_SITE = $(call github,any1,neatvnc,v$(NEATVNC_VERSION))
 NEATVNC_LICENSE = ISC
 NEATVNC_LICENSE_FILES = COPYING
